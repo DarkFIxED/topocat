536c8db1cbbfb6e7f295179339c33b1a895d95c5
mikhalkov <fixed.fred@gmail.com>
2019-01-13 22:08:56 +0700

    TC-12: updated area domain model

--- Topocat.Frontend/src/app/domain/map/area.ts
+++ Topocat.Frontend/src/app/domain/map/area.ts
@@ -1,10 +1,20 @@
-import { MapObject } from './map-object';
 import { JsonObject, JsonProperty } from 'json2typescript';
 import { Coords } from './coords';
+import { NameableMapObject } from './nameable-map-object';
 
 @JsonObject('area')
-export class Area extends MapObject {
+export class Area extends NameableMapObject {
 
     @JsonProperty('path', [Coords])
     public path: Coords[];
+
+    public constructor(title?: string, description?: string, path?: Coords[]) {
+        super(title, description);
+
+        if (!path) {
+            this.path = [];
+        } else {
+            this.path = path;
+        }
+    }
 }
