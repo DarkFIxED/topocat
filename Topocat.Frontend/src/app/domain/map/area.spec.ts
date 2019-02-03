74a39822e351d368edc20e82880f58f8e7b99b7e
mikhalkov <fixed.fred@gmail.com>
2019-02-03 21:35:36 +0700

    Added area tests

--- Topocat.Frontend/src/app/domain/map/area.spec.ts
+++ Topocat.Frontend/src/app/domain/map/area.spec.ts
@@ -1,8 +1,11 @@
 import { Coords } from './coords';
-import { Path } from './path';
 import { Area } from './area';
 
-describe('Path', () => {
+const delay = ms => new Promise((resolve) => {
+    setTimeout(resolve, ms);
+});
+
+describe('Area', () => {
     it('constructor(coords[]) creates new Area instance with pre-filled path', () => {
 
         // Arrange.
@@ -28,7 +31,7 @@ describe('Path', () => {
         let anotherArea = new Area(title, desc, coords);
 
         let area = new Area();
-        area.changed.subscribe(()=> {
+        area.changed.subscribe(() => {
             expect().nothing();
         });
 
@@ -44,4 +47,45 @@ describe('Path', () => {
             return pathEl.lat === coords[index].lat && pathEl.lng === coords[index].lng;
         })).toBeTruthy();
     });
+
+    it('merge() throws error when current object newest than another', async () => {
+
+        let anotherArea = new Area();
+        await delay(10);
+        let currentArea = new Area();
+
+        // Act.
+        expect(function () {
+            currentArea.merge(anotherArea);
+        }).toThrowError();
+    });
+
+    it('merge() copies all data except uuid from another object and fires changed observable', async () => {
+
+        // Arrange.
+        let currentArea = new Area();
+
+        await delay(10);
+
+        let title = 'title';
+        let desc = 'desc';
+        let coords = [new Coords(), new Coords()];
+        let anotherArea = new Area(title, desc, coords);
+        currentArea.changed.subscribe(() => {
+            expect().nothing();
+        });
+
+        // Act.
+        currentArea.merge(anotherArea);
+
+        // Assert.
+        expect(currentArea.title).toEqual(anotherArea.title);
+        expect(currentArea.description).toEqual(anotherArea.description);
+        expect(currentArea.uuid).not.toEqual(anotherArea.uuid);
+        expect(currentArea.lastModifiedDate).toEqual(anotherArea.lastModifiedDate);
+        expect(currentArea.path.coords.every((pathEl, index) => {
+            return pathEl.lat === coords[index].lat && pathEl.lng === coords[index].lng;
+        })).toBeTruthy();
+    });
+
 });
