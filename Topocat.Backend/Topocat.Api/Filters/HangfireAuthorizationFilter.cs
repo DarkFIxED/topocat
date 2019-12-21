92c52d2e17c51f37330cfce64bf41838802aa255
DarkFIxED <fixed.fred@gmail.com>
2019-12-21 14:53:31 +0300

    Added config for staging and ignoring env files

---
+++ Topocat.Backend/Topocat.Api/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire.Dashboard;
+
+namespace Topocat.API.Filters
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return true; //httpContext.User.Identity.IsAuthenticated;
+        }
+    }
+}
